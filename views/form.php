29223cfc948e272cfea1d4eabe9a3e66bb1acf0b
Danil Chobulda <dan.chobulda@gmail.com>
2022-08-18 09:49:52 +0300

    Delete form.php

--- views/form.php
+++
@@ -1,8 +0,0 @@
-<button type="button" class="btn btn-primary" id="add" data-toggle="modal" data-target="#user-form-modal">Add</button>
-<select class="form-select option-select" aria-label="Пример выбора по умолчанию">
-  <option selected>Please select</option>
-  <option value="active">Set active</option>
-  <option value="not-active">Set not active</option>
-  <option value="delete">Delete</option>
-</select>
-<button type="button" class="btn btn-primary checkbox-action">Ok</button>
